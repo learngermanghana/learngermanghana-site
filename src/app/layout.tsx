0b6290a290a949f05b83c3bf6aae3d7193d135fd
learngermanghana <learngermanghana@gmail.com>
2026-02-11 14:47:29 +0100

    Add Google Ads gtag script to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -129,6 +129,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11200479914" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'AW-11200479914');`,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
